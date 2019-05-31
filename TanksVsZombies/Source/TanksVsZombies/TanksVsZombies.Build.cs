e495bba76bbe59c4fe40e96038aa03c04a55628a
oO-Blink-Oo <alvarezian23@gmail.com>
2019-05-31 13:12:02 -0700

    added "Paper2d" in the .Build.cs

--- TanksVsZombies/Source/TanksVsZombies/TanksVsZombies.Build.cs
+++ TanksVsZombies/Source/TanksVsZombies/TanksVsZombies.Build.cs
@@ -8,7 +8,7 @@ public class TanksVsZombies : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2d" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
